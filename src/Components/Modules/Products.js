5741d07c66c4164e81b27a3f851f59b7dd5721e6
muppettMP <rowerybikelpl@gmail.com>
2021-11-28 22:26:09 +0100

    Finish transfer of JS DOM functions from skeleton

--- src/Components/Modules/Products.js
+++ src/Components/Modules/Products.js
@@ -19,6 +19,23 @@ export default function Products(props) {
     "Uwagi",
   ];
 
+  const dummyCells = [
+    "cb",
+    "KONWERTER USB - RS232 UART FT232RL FTDI",
+    "szt.",
+    "1000",
+    "1000",
+    "1000",
+    "1000",
+    "22,35",
+    "10001",
+    "TME Sp z o.o.",
+    "CB-HFT6.4/1M-RD",
+    "Surowiec",
+    "MODUŁY",
+    "Długi termin",
+  ];
+
   function headingRow() {
     return headingCells.map((cell) => {
       if (cell === "cb") {
@@ -33,5 +50,32 @@ export default function Products(props) {
     });
   }
 
-  return <Row heading={true}>{headingRow()}</Row>;
+  function dummyRow() {
+    return dummyCells.map((cell) => {
+      if (cell === "cb") {
+        return <Cell key={cell} type="checkbox" name={cell} id={cell} />;
+      } else {
+        return (
+          <Cell key={Math.floor(Math.random() * 1000000)} name={cell} id={cell}>
+            {cell}
+          </Cell>
+        );
+      }
+    });
+  }
+
+  function dummyRows() {
+    let rows = [];
+    for (let i = 0; i < 100; i++) {
+      rows.push(<Row key={i}>{dummyRow()}</Row>);
+    }
+    return rows;
+  }
+
+  return (
+    <>
+      <Row heading={true}>{headingRow()}</Row>
+      {dummyRows()}
+    </>
+  );
 }
